840cf8a7770573f54bb5acfd0fd67558823bf196
Liam <liamdudley@live.ie>
2021-02-10 23:03:20 +0000

    created work app

--- signature/settings.py
+++ signature/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'work.apps.WorkConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- signature/urls.py
+++ signature/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('work/', include('work.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
