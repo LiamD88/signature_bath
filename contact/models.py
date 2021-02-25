38beae4b346ce959ef0c5be21cb19447554b1f6d
Liam <liamdudley@live.ie>
2021-02-25 20:16:14 +0000

    created contact form and created model to save to database

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Contact(models.Model):
+
+    """model to save contact form details"""
+
+    name = models.CharField(max_length=150)
+    number = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField(max_length=1000)
+ 
+    time = models.DateTimeField(default=timezone.now)
+
+
+
+    def __str__(self):
+        return self.name
