e458cfb19b1c5bf3a2d95ed01147273008fcd9d4
Liam <liamdudley@live.ie>
2021-04-02 20:46:32 +0100

    amended contact form to save details to database

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from .models import Contact
+from .forms import ContactForm
 
 def contact(request):
+
+    if request.method == 'POST':
+
+        form = Contact(    
+            name = request.POST.get('name'),
+            message = request.POST.get('message'),
+            email = request.POST.get('email')
+        )
+        form.save()
 
     form = ContactForm
 
--- signature/settings.py
+++ signature/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'u=+!gq^-guptjq5=da2@hai9w_(+6a)22mz!2$*7bv%a+^jo0u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
