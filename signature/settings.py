bfefc576c283173b72c118602206d464509200f2
LiamD88 <50365210+LiamD88@users.noreply.github.com>
2022-08-07 19:47:24 +0100

    changed degug mode to false

--- signature/settings.py
+++ signature/settings.py
@@ -29,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',
-'signatute-bath.herokuapp.com']
+ALLOWED_HOSTS = ['signatute-bath.herokuapp.com',
+                '127.0.0.1',]
 
 
 # Application definition
