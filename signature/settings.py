bd3936f6a79458745d77e82c3b2e85d4442e940f
LiamD88 <50365210+LiamD88@users.noreply.github.com>
2022-08-15 19:51:04 +0100

    updated settings

--- signature/settings.py
+++ signature/settings.py
@@ -19,7 +19,7 @@ if os.path.exists('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
